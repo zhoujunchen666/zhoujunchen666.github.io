fe1f30fa2f6880d013b3ea1c005ab8e7fee3bbdb
zhoujunchen666 <qq3681001837@outlook.com>
2025-08-03 21:57:19 +0800

    Site updated: 2025-08-03 21:57:19

---
+++ js/fixed-bg.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const bg = document.querySelector('.banner-inner');
+    if (bg) {
+        bg.style.backgroundAttachment = 'fixed';
+    }
+});
