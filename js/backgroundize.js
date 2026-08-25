ca74be82d5f951c41700fbd74312b355295a21b7
zhoujunchen666 <qq3681001837@outlook.com>
2026-08-25 21:28:11 +0800

    Site updated: 2026-08-25 21:28:11

--- js/backgroundize.js
+++ js/backgroundize.js
@@ -3,7 +3,7 @@ const viewBg = $("#web_bg");
 const bannerMask = $("#banner .mask");
 const bg = $(bannerContainer).css("background-image");
 $(viewBg).css("background-image", bg);
-$(bannerContainer).css("background-image", "url()");
+$(bannerContainer).css("background-image", "url(https://cdn.luogu.com.cn/upload/image_hosting/gpua9m0b.png)");
 const color = $(bannerMask).css("background-color");
 $(bannerMask).css("background-color", `rgba(0,0,0,0)`);
 $(viewBg).css("background-color", color);
